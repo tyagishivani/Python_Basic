3ef965bd6e6af6b64ef6d19f41f5122ecc230aa9
tyagishivani <68904446+tyagishivani@users.noreply.github.com>
2020-07-28 17:44:39 +0530

    Add files via upload

---
+++ reverse_name.py
@@ -0,0 +1,4 @@
+first_name = input('Enter the First name')
+last_name = input('Enter the Last name')
+print(first_name[::-1] + " "+last_name[::-1])
+
---
+++ vol_sphere.py
@@ -0,0 +1,6 @@
+
+import math
+D = 12
+R = D/2
+vol_sphere = 4/3 * math.pi * R * R
+print(vol_sphere)
